d9b2f172ae97f603a5859404d10008a632597308
unknown <michaelgardone@gmail.com>
2024-04-02 18:33:00 -0600

    fixed issues with PCC and Unity, PCC is written in C#10 and Unity wants C#9

---
+++ Unity/Assets/Libraries/PCC/Utility/Memory/IMemoryBuffer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PCC.Utility.Memory
+{
+    public interface IMemoryBuffer<T>
+    {
+        public void Add(T item);
+
+        public T Get(int indx);
+
+        public void Replace(int indx, T item);
+
+        public void Clear();
+
+        public bool IsFull();
+
+        public int Count();
+        public int Size();
+    }
+}
